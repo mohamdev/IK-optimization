22c8695d233c60176965b553b2be685905174ed1
mohamdev <adjelmohamed@gmail.com>
2020-10-22 06:27:36 +0200

    Remove duplicated directory

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lpython2.7 -lpinocchio -lboost_filesystem -lboost_serialization -lboost_system -lipopt -lcoinmumps -ldl -llapack -lblas -lm
-
--- Debug/src/test-pinocchio-fkine.d
+++
@@ -1,1 +0,0 @@
-src/test-pinocchio-fkine.o: ../src/test-pinocchio-fkine.cpp
